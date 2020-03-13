8f21e9663f29c25aae5ffa5d486a63d2a11076f6
umbur <you@example.com>
2020-03-13 10:43:12 -0600

    completes ex1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -12,6 +12,13 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
+    for i in range(length):
+        main =  hash_table_retrieve(ht, limit - weights[i])
+        if main is not None: 
+            answer = (i, main)
+            return answer
+        else: 
+            hash_table_insert(ht, weights[i], i)
 
     return None
 
